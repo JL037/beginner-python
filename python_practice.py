f8bd6f32f4271920231d79fc7a98ea9319a9370b
Jared Lemler <jaredlemler@Kaitlins-MacBook-Air.local>
2025-04-22 16:30:10 -0400

    updated

---
+++ python_practice.py
@@ -0,0 +1,68 @@
+age = 25
+print(age)
+
+age1 = 1
+age_2 = 2
+age_of_user = 7
+
+
+result = 20 / 3
+print(result)
+
+print("Here is a floor version (crop decimal):", 20 // 3)
+
+result = 5 ** 3
+print(result)
+
+print(round(20 / 3, 0))
+
+print(int(20 / 3))
+
+print(result / age)
+
+print(20 / 3 + 1)
+
+print((20 / 3) + 1)
+
+print(20 / (3 + 1))
+
+print(5 ** 5)
+
+print(78 % 11)
+
+age = 30
+age += 1
+print(age)
+
+age = 300
+age /= 9
+print(age)
+
+me = "Jared"
+print("/n")
+
+me = "J\ta\tr\te\td"
+print(me)
+
+you = 'John'
+me = "Jared"
+print(me, you)
+
+single_quotes = 'She said "Hi"'
+print(single_quotes)
+
+double_quotes = "She said \"Hi\""
+print(double_quotes)
+
+single_quotes = 'I\'m learning!'
+print(single_quotes)
+
+double_quotes = "I'm learning!"
+print(double_quotes)
+
+print(r"as raw as I\'ve ever seen. \/\/ () \/\/. \t")
+
+msg = me + " + " + you
+print(msg)
+
+print(me, "+", you)
